3afa21035f449009864ca30a72671000db783ba2
Kathryn Andersen <rubykat@cpan.org>
2005-05-12 11:45:25 +0000

    Import of RUBYKAT/Posy-Plugin-FindGrep-0.22 from CPAN.
    gitpan-cpan-distribution: Posy-Plugin-FindGrep
    gitpan-cpan-version:      0.22
    gitpan-cpan-path:         RUBYKAT/Posy-Plugin-FindGrep-0.22.tar.gz
    gitpan-cpan-author:       RUBYKAT
    gitpan-cpan-maturity:     released

--- lib/Posy/Plugin/FindGrep.pm
+++ lib/Posy/Plugin/FindGrep.pm
@@ -7,11 +7,11 @@ Posy::Plugin::FindGrep - Posy plugin to find files using grep.
 
 =head1 VERSION
 
-This describes version B<0.21> of Posy::Plugin::FindGrep.
+This describes version B<0.22> of Posy::Plugin::FindGrep.
 
 =cut
 
-our $VERSION = '0.21';
+our $VERSION = '0.22';
 
 =head1 SYNOPSIS
 
